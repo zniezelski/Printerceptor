7127c12adf262d8567e69d84a44efb5bf4ac64c5
zniezelski <zniezelski@lvtech.net>
2016-05-07 11:25:04 -0700

    3.1

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ xcopy /e /i "PSTerminalServices" "C:\Windows\system32\WindowsPowerShell\v1.0\Mod
 schtasks /delete /tn "Printerceptor" /f
 schtasks /create  /tn "Printerceptor" /XML ./tasks/printerceptor.xml
 schtasks /create  /tn "Printerceptor - isolate" /XML "./tasks/Printerceptor - Isolate.xml"
-mkdir "C:\programdata\microsoft\Windows\Start Menu\Programs\Printerceptor"
-xcopy "C:\Program Files\Printerceptor\printerceptor.lnk" "C:\programdata\microsoft\Windows\Start Menu\Programs\Printerceptor"
+mkdir "C:\programdata\microsoft\Windows\Start Menu\Programs\Printerceptor" /y
+xcopy "C:\Program Files\Printerceptor\printerceptor.lnk" "C:\programdata\microsoft\Windows\Start Menu\Programs\Printerceptor" /y
 
 cscript MessageBox.vbs "Printerceptor UI will now open. It may take a few seconds to load..."
